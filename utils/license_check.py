29a7c3b30077ad489754baaf1e6202dad28617df
Gold3xp <117042030+Gold3xp@users.noreply.github.com>
2025-07-24 17:43:03 +0700

    license_check.py

--- utils/license_check.py
+++ utils/license_check.py
@@ -1,3 +1,7 @@
+import os
+
+LICENSE_FILE = "license.key"
+
 def is_license_valid(key: str) -> bool:
     try:
         with open("valid_keys.txt", "r") as file:
@@ -6,3 +10,13 @@ def is_license_valid(key: str) -> bool:
     except FileNotFoundError:
         print("❌ File valid_keys.txt tidak ditemukan.")
         return False
+
+def get_or_create_license():
+    if os.path.exists(LICENSE_FILE):
+        with open(LICENSE_FILE, "r") as f:
+            return f.read().strip()
+    else:
+        key = input("🔑 Masukkan license key: ").strip()
+        with open(LICENSE_FILE, "w") as f:
+            f.write(key)
+        return key
